8094de5721b1ee8ab70a8cb827ae46f70af1f07c
Sam <leon36@gmail.com>
2026-07-02 15:01:08 -0500

    Revert "test: TEMP throwaway packet-path regression (demonstrates netns job goes red)"
    This reverts commit 4499eb89e57a516d26fedb40d15496db1e8dbeac.

--- tests/test_netns_harness.py
+++ tests/test_netns_harness.py
@@ -157,7 +157,7 @@ _ZONES = (
 )
 def test_forward_drop_blocks_ping() -> None:
     accept = Ruleset(
-        zones=_ZONES, policies=(Policy(source="client", dest="server", action="DROP"),)
+        zones=_ZONES, policies=(Policy(source="client", dest="server", action="ACCEPT"),)
     )
     drop = Ruleset(
         zones=_ZONES, policies=(Policy(source="client", dest="server", action="DROP"),)
